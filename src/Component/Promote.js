10c5859a95abe1580dec13e22e0cffec17b322db
liuzhenangel <550686383@qq.com>
2016-10-10 10:18:58 +0800

    update url

--- src/Component/Promote.js
+++ src/Component/Promote.js
@@ -9,7 +9,7 @@ export default class Promote extends Component {
         </div>
         <ul className="list-group">
           <li className="list-group-item">
-            <a href='http://react.liuzhen.me' target='_blank'>RubyChina 克隆 | React.js</a>
+            <a href='http://ruby-china.liuzhen.me' target='_blank'>RubyChina 克隆 | React.js</a>
           </li>
           <li className="list-group-item">
             <a href='http://v2ex.liuzhen.me' target='_blank'>V2EX 克隆 | Vue.js</a>
